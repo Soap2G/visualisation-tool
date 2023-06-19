43c79c3d487a2655f02478d3cefab041c164f0d7
gguerrie <giovanni.guerrieri@cern.ch>
2023-06-19 18:37:42 +0200

    initial commit

---
+++ pages/compare/cards_compare/wealth_indexes_chart.py
@@ -0,0 +1,25 @@
+import dash_bootstrap_components as dbc
+import dash_daq as daq
+from dash import dcc, html
+
+card_graf_compare = dbc.Card(
+    dbc.CardBody(
+        [
+            dcc.Loading(
+                [
+                    dcc.Graph(id="al-wealth-indexes"),
+                    html.Div(
+                        daq.BooleanSwitch(
+                            id="logarithmic-scale-switch",
+                            on=False,
+                            label="Logarithmic Y-Scale",
+                            labelPosition="bottom",
+                        ),
+                        id="al-logarithmic-scale-switch-div",
+                    ),
+                ],
+            )
+        ]
+    ),
+    class_name="mb-3",
+)
---
+++ pages/efficient_frontier/cards_efficient_frontier/ef_chart.py
@@ -0,0 +1,12 @@
+from dash import html
+import dash_bootstrap_components as dbc
+from dash import dcc
+
+card_graf = dbc.Card(
+    dbc.CardBody(
+        [
+            html.Div(dcc.Loading(dcc.Graph(id="ef-graf")))
+        ]
+    ),
+    class_name="mb-3",
+)
